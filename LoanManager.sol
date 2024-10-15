5f1e752c1398cc589161012136f44c6712df5971
eliab256 <bordonielia96@gmail.com>
2024-10-15 18:09:08 +0200

    creazione Enum e struct Loan

--- LoanManager.sol
+++ LoanManager.sol
@@ -1,7 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.0;
 
 contract LoanManager{
     
     mapping(address => uint) public usersBalances ;
+
+    enum LoanStatus{
+        Active,
+        Pending,
+        Paid,
+        Overdue
+    }
+
+    struct Loan{
+        address borrowerAddress;
+        address lenderAddress;
+        uint256 amount;
+        uint InterestRate; //in percentuale usando una scala di 100 es: 525 = 5,25%
+        uint PenaltyRate;
+        uint startDate; //usare timestamp 
+        uint dueDate;
+        bool isPaid;
+        LoanStatus status;
+    }
+
+
+
 }
