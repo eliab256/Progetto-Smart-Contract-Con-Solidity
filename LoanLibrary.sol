6cd0e70297a53f8c7c26f718a606fbcd3820aba8
eliab256 <bordonielia96@gmail.com>
2024-10-17 21:57:46 +0200

    inzio creazione funzione per richiedere il prestito

--- LoanLibrary.sol
+++ LoanLibrary.sol
@@ -1,23 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.0;
 
 library LoanLibrary{
     function dailyInterestRateCalculator(uint interestRate, uint loanAmount )  internal pure returns(uint){
-        return (loanAmount * interestRate)/(100*365);
+        uint dailyInterestRate = (loanAmount * interestRate)/(100*365);
+        return dailyInterestRate;
     }
 
-   function dailyPenaltyRateCalculator(uint penaltyRate, uint loanAmount, uint dailydelay) internal pure returns(uint){
-        return loanAmount*(1 + (penaltyRate / 100 / 365)) ** (365 * dailydelay / 365);
+    function dailyPenaltyRateCalculator(uint penaltyRate, uint loanAmount, uint dailydelay) internal pure returns (uint) {
+        uint penaltyAmount = (loanAmount * penaltyRate * dailydelay) / (100 * 365);
+        return penaltyAmount;
     }
  
 }
 
-contract LoanLibraryContract{
-     function calculateDailyInterest(uint interestRate,uint loanAmount) public pure returns (uint) {
+contract LoanLibraryContract {
+    function calculateDailyInterest(uint interestRate, uint loanAmount) public pure returns (uint) {
         return LoanLibrary.dailyInterestRateCalculator(interestRate, loanAmount);
     }
 
-    function calculateDailyPenalty(uint penaltyRate,uint loanAmount, uint dailydelay) public pure returns (uint) {
+    function calculateDailyPenalty(uint penaltyRate, uint loanAmount, uint dailydelay) public pure returns (uint) {
         return LoanLibrary.dailyPenaltyRateCalculator(penaltyRate, loanAmount, dailydelay);
     }
 }
--- LoanManager.sol
+++ LoanManager.sol
@@ -1,12 +1,13 @@
+
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
+
 import "./LoanLibrary.sol";
 
 contract LoanManager{
-    using LoanLibrary for uint256;
 
-    mapping(address => uint) public usersBalances ;
-
+    //variables and structs
     enum LoanStatus{
         Active,
         Pending,
@@ -14,21 +15,76 @@ contract LoanManager{
         Overdue
     }
 
-    struct Loan{
+    struct LoanBorrower{
         address borrowerAddress;
+        uint8 creditScore;
+        uint256[] userLoans;
+    }
+
+    struct Loan{
+        uint256 loanId;
+        LoanBorrower borrowerAddress;
         address lenderAddress;
         uint256 amount;
-        uint InterestRate; //in percentuale usando una scala di 100 es: 525 = 5,25%
-        uint PenaltyRate;
-        uint startDate; //usare timestamp 
-        uint dueDate;
-        bool isPaid;
+        uint256 interestRate; //in percentuale usando una scala di 100 es: 525 = 5,25%
+        uint256 penaltyRate;
+        uint256 startDate; //usare timestamp 
+        uint256 dueDate;
         LoanStatus status;
     }
 
+    uint256 private loanCounter;
+
+    mapping(uint256 => Loan) public loansById;
+    mapping(address => uint256[]) public userLoans;
+
+    //events
+    event LoanRequested(uint256 loanId, address borrower, uint256 amount, LoanStatus status);
+
+    //functions
+    function requestLoan(uint256 _amount, uint256 _daysOfLoan) public {
+        //requires
+        require(_amount > 0, "The loan amount must be greater than zero");
+        require(-_daysOfLoan > 0, "The loan period must be grater than zero days");
+        uint256[] memory loans = userLoans[msg.sender];
+
+        for (uint i = 0; i < loans.length; i++) {
+            uint256 loanId = loans[i];
+            Loan memory userLoan = loansById[loanId];
+
+        require(
+            userLoan.status == LoanStatus.Paid, "You cannot request a new loan if you have pending, active, or overdue loans");
+        }
+        
+        //actions
+        loanCounter++;
+        uint numberOfLoans = userLoans[msg.sender].length;
+        uint borrowerCreditScore = LoanBorrower.creditScore;
+        uint interestRateCalculation;
+        
+      
+        if(numberOfLoans == 0){
+            borrowerCreditScore = 1;
+        }
+
+        function getInterestRate(_daysOfLoan, borrowerCreditScore) private {
+            
+        }
+
+
+        Loan memory newLoan = Loan({
+            loanId: loanCounter,
+            borrowerAddress: LoanBorrower(msg.sender, 1, userLoans + 1 ), // Inizializza il borrower con un credit score di 1 e un array vuoto
+            lenderAddress: address(0),  // Nessun prestatore ancora assegnato
+            amount: _amount,
+            interestRate: 0,  
+            penaltyRate: 0,  
+            startDate: 0,  
+            dueDate: 0,  
+            status: LoanStatus.Pending 
+        });
+
+    };
 
 
 }
-
-
-
